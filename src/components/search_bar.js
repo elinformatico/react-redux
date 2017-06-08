6eb185aa8a66e60f93478e4bac86a47cb0ec532b
elinformatico <color.ui>
2017-06-08 15:13:00 -0500

    Refactoring the function Component to a Class Component and also implementing a state for App Class Component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,7 @@ class SearchBar extends Component
 {
     constructor(props) {
         super(props);
-        this.state = { term : 'Ola ke ase xD' };
+        this.state = { term : '' };
     }
 
     render () {
--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyAEer9wsuF8Bm9LpFV-pNM5nLS0vHR3MRY';
 
-// Calling a REST API from Youtube API
-YTSearch({ key : API_KEY, term : 'Playstation 4'}, function(response){
-    console.log(response);
-});
+class App extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = { videos : [] };
 
-// Step to create a Components
-// 1) Create a new Component, this Component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        YTSearch({ key : API_KEY, term : 'Playstation 4'}, (videos) => {
+            this.setState({ videos });  // Instead of this.setState({ videos : videos });
+        });
+    }
+
+    render () {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
-// 2) Take this Component's generated HTML and put it on the page (In the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
