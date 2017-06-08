8a4638f31e4bdda154dcc4b39cbd9f42de9129d2
elinformatico <color.ui>
2017-06-08 13:01:50 -0500

    Adding a initial state to the term object value

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,7 @@ class SearchBar extends Component
 {
     constructor(props) {
         super(props);
-        this.state = { term : '' };
+        this.state = { term : 'Ola ke ase xD' };
     }
 
     render () {
