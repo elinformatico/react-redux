95aee460216d3c7a2f9606ef114418d53a7bdb7e
elinformatico <color.ui>
2017-06-08 12:28:28 -0500

    Adding a New Class extend React.Component and also add a handle event

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,1 +1,24 @@
- 
+import React, { Component } from 'react';
+
+/*const SearchBar = () => {
+    return <input />;
+}*/
+
+class SearchBar extends Component
+{
+    constructor(props) {
+        super(props);
+        this.state = { terms : '' };
+    }
+
+    render () {
+        // return <input onChange={this.onInputChange} />;
+        return <input onChange={(event) => console.log(event.target.value)} />;
+    };
+
+    /*onInputChange(event){
+        console.log(event, ' --> Target Value: ' + event.target.value);
+    }*/
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAEer9wsuF8Bm9LpFV-pNM5nLS0vHR3MRY';
 
 // Step to create a Components
 // 1) Create a new Component, this Component should produce some HTML
 const App = () => {
-  return <h3>Hi, Welcome to the REACT Library, Just another great JS Library</h3>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // 2) Take this Component's generated HTML and put it on the page (In the DOM)
