f94a63b75458388a683d103bfa299bdeb3343fca
elinformatico <color.ui>
2017-06-08 15:36:25 -0500

    Implementing the video_list Component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,1 +1,11 @@
- 
+import React from 'react';
+
+const VideoList = (props) => {
+    return (
+        <ul className="col-md-4 list-group">
+            {props.videos.length} Videos
+        </ul>
+    );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAEer9wsuF8Bm9LpFV-pNM5nLS0vHR3MRY';
 
 class App extends Component
@@ -20,6 +21,7 @@ class App extends Component
         return (
             <div>
                 <SearchBar />
+                <VideoList videos = {this.state.videos} />
             </div>
         );
     }
