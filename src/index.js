5d07136fc410abe7c01734a342e22c9117960c9f
elinformatico <color.ui>
2017-06-08 10:30:17 -0500

    Adding the Youtube API_KEY

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Step to create a Components
+const API_KEY = 'AIzaSyAEer9wsuF8Bm9LpFV-pNM5nLS0vHR3MRY';
 
+// Step to create a Components
 // 1) Create a new Component, this Component should produce some HTML
 const App = () => {
-  return <h2>Hi, Welcome to the REACT Library, Just another great JS Library</h2>
+  return <h3>Hi, Welcome to the REACT Library, Just another great JS Library</h3>
 }
 
 // 2) Take this Component's generated HTML and put it on the page (In the DOM)
