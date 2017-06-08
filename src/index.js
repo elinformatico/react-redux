7026221faf9655df4f5505be1a0e98e51ef7517b
elinformatico <color.ui>
2017-06-08 14:50:50 -0500

    Importing a youtube API rest plugin and also it is implementing a simple call function rest to fetch data from youtube

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAEer9wsuF8Bm9LpFV-pNM5nLS0vHR3MRY';
+
+// Calling a REST API from Youtube API
+YTSearch({ key : API_KEY, term : 'Playstation 4'}, function(response){
+    console.log(response);
+});
 
 // Step to create a Components
 // 1) Create a new Component, this Component should produce some HTML
