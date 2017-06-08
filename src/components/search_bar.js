3f455ff42412634c11b9148f443807d7efba2bbb
elinformatico <color.ui>
2017-06-08 12:41:30 -0500

    Handling the Event onChange and also a state to render the value when the event change

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 
-/*const SearchBar = () => {
-    return <input />;
-}*/
-
 class SearchBar extends Component
 {
     constructor(props) {
         super(props);
-        this.state = { terms : '' };
+        this.state = { term : '' };
     }
 
     render () {
-        // return <input onChange={this.onInputChange} />;
-        return <input onChange={(event) => console.log(event.target.value)} />;
+        return (
+            <div>
+                <input onChange={event => this.setState({ term : event.target.value })} />
+                Value of the input: { this.state.term }
+            </div>
+        );
     };
-
-    /*onInputChange(event){
-        console.log(event, ' --> Target Value: ' + event.target.value);
-    }*/
 }
-
 export default SearchBar;
