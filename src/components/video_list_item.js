ba1631c3632d1f4af54f4e42d8facb14a3b8d395
elinformatico <color.ui>
2017-06-21 14:49:03 -0500

    Removing the console.log

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,7 +3,6 @@ import React from 'react';
 const VideoListItem = (props) =>
 {
     var urlYoutube = 'https://www.youtube.com/watch?v=' + props.video.id.videoId;
-    console.log(props.video);
     return (
         <li>
             <a href={urlYoutube} alt={props.video.snippet.title}> {props.video.snippet.title} </a>
